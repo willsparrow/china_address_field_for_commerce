32c22e9001e92869c6336e4a1de4d654d99dd164
willsparrow <406061801@qq.com>
2014-08-29 13:37:01 +0800

    add product selected style

--- ml/sites/all/themes/bootstrap-business/js/product.js
+++ ml/sites/all/themes/bootstrap-business/js/product.js
@@ -3,12 +3,12 @@
 		attach: function() {
 			$('.container-product-quanlity div.icon-product-add').click(function(){
 				var quanlityInput = $(this).siblings('div.form-item-quantity').children('input.form-text');
-				console.log(quanlityInput);
+				//console.log(quanlityInput);
 				quanlityInput.val(parseInt(quanlityInput.val()) + 1);
 			});
 			$('.container-product-quanlity div.icon-product-del').click(function(){
 				var quanlityInput = $(this).siblings('div.form-item-quantity').children('input.form-text');
-				console.log(quanlityInput);
+				//console.log(quanlityInput);
 				quanlityInput.val(parseInt(quanlityInput.val()) - 1);
 			});
 		}
